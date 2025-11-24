07c388f87a27c9bb7e5b4146cb030bdbe909a23f
Nitesh <nitesh.e21@nsut.ac.in>
2025-11-24 10:18:20 +0530

    :q `

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+
+
+mongoose.connect("mongod://127.0.0.1:27017/test")
+.then(()=>console.log("db connected"))
+app.listen(4334,()=>{
+    console.log("server started")
+})
