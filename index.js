531a8185f47ee71f7314c7f30f0febd8bad0e99d
Nitesh <nitesh.e21@nsut.ac.in>
2025-11-26 11:50:28 +0530

    delete blog router

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
-app.use("/api/users",require("./routes/user.routes"))
+
 mongoose.connect("mongod://127.0.0.1:27017/test")
 .then(()=>console.log("db connected"))
 app.listen(4334,()=>{
--- routes/blog.router.js
+++
@@ -1,7 +0,0 @@
-const router = require("express").Router();
-router.get("/getblogs",(req,res)=>{
-    res.json({
-        success:true,
-        data:[{title:"Myblog"},{title:"second blog"}]
-    })
-})
