4a0491b71c275f55c88752eff490bc8a55a00003
Nitesh <nitesh.e21@nsut.ac.in>
2025-11-26 11:38:38 +0530

    get all blogs route created

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ const mongoose = require("mongoose");
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
-app.use("/api/blogs",require("./routes/blog.router"))
+app.use("/api/blog",require("./routes/blog.routes"))
+
 mongoose.connect("mongod://127.0.0.1:27017/test")
 .then(()=>console.log("db connected"))
 app.listen(4334,()=>{
